2501eb97461989ee2b76a7bf34758bb6e1aede72
jclanas2019 <49561939+jclanas2019@users.noreply.github.com>
2024-10-06 18:58:23 -0300

    Add files via upload

--- scripts/deploy_k8s.sh
+++ scripts/deploy_k8s.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
 
+# Aplicar configuraciones de RBAC
+kubectl apply -f config/rbac.yaml
+
+# Desplegar los agentes en Kubernetes
+kubectl apply -f kubernetes/agent-deployment.yaml
+
+# Desplegar Prometheus para el monitoreo
+kubectl apply -f kubernetes/prometheus-config.yaml
